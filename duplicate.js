b05a60d3f2ec0a6f8200776e78638959e2830378
Sakib Hasan <82746139+sakibhasansufi@users.noreply.github.com>
2024-12-10 22:57:19 +0600

    Update duplicate.js

--- duplicate.js
+++ duplicate.js
@@ -3,8 +3,14 @@ Write a function to remove the duplicate value
 */
 
 function removeDuplicate (arr) {
-	let newarr = [...new Set(arr)];
-	return newarr;
+	let uniqueValue = [];
+	for(let i=0; i< arr.length; i++){
+		let newElement = arr[i];
+		if(!uniqueValue.includes(newElement)){
+			uniqueValue.push(newElement)
+		}	
+	}
+	return uniqueValue;
 }
 
 console.log(removeDuplicate([3,3,31,45,7,4,4]));
